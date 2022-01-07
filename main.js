4e6037d1e96a24a7ced67b27930637914ae8d941
pantchayan <pantchayan@gmail.com>
2022-01-07 11:26:15 +0530

    Updated Fragment Shaders and Vertex Shaders

--- main.js
+++ main.js
@@ -3,8 +3,103 @@ import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/exampl
 let audioCtx;
 let analyser;
 let audioSrc;
-let mode = "red"; // red or #ff00ff
+// let mode = "#ff00ff"; // red or #ff00ff
 let songNum = 0;
+
+const planeUniforms = {
+  iTime: { value: 0 },
+  iResolution: { value: new THREE.Vector3() },
+};
+
+const planeVertexShader = `
+#include <common>
+attribute float aFrequency;
+varying float vFrequency;
+varying vec4 vPosition;
+void main()
+{
+    
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.y += (aFrequency - 150.0) * 0.15 - 0.5;
+    
+    vPosition = modelPosition;
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+
+    vFrequency = aFrequency;
+}`;
+
+const planeFragmentShader = /*  */ `
+#include <common>
+uniform vec3 iResolution;
+uniform float iTime;
+varying vec4 vPosition;
+// By iq: https://www.shadertoy.com/user/iq  
+// license: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+ 
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    col.rgb -=  abs(7.0 - vPosition.y - 2.0)/25.0;
+    // Output to screen
+    fragColor = vec4(col,1.0);
+}
+ 
+void main() {
+  mainImage(gl_FragColor, gl_FragCoord.xy);
+}`;
+
+const shapeVertexShader = `
+#include <common>
+
+uniform vec3 iResolution;
+uniform float iTime;
+attribute float aFrequency;
+varying vec4 vPosition;
+void main()
+{
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    if(aFrequency > 100.0){
+        modelPosition.y *= aFrequency * 0.01;
+    }
+    
+    vPosition = modelPosition;
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+}`;
+
+const shapeFragmentShader = /*  */ `
+#include <common>
+uniform vec3 iResolution;
+uniform float iTime;
+varying vec4 vPosition;
+// By iq: https://www.shadertoy.com/user/iq  
+// license: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+ 
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    col.rgb -=  abs(7.0 - vPosition.y - 2.0)/25.0;
+    // Output to screen
+    fragColor = vec4(col,1.0);
+}
+ 
+void main() {
+  mainImage(gl_FragColor, gl_FragCoord.xy);
+}`;
+
 document.querySelector(".overlay").addEventListener("click", () => {
   // console.log(audioCtx);
   audioCtx = new (window.AudioContext || window.webkitAudioContext)();
@@ -20,15 +115,15 @@ document.querySelector(".overlay").addEventListener("click", () => {
   console.log(audioCtx);
   audioSrc.connect(analyser);
   audioSrc.connect(audioCtx.destination);
-  document.getElementById('change-btn').classList.remove('hide')
-  document.getElementById("test-song").classList.remove('hide')
+  document.getElementById("change-btn").classList.remove("hide");
+  document.getElementById("test-song").classList.remove("hide");
 });
 
 // setInterval(() => {
 //   document.getElementById("test-song").play();
 // }, 500)
 
-const frequencyData = new Uint8Array(210);
+const frequencyData = new Uint8Array(1024);
 
 const sizes = { height: window.innerHeight, width: window.innerWidth };
 const canvas = document.querySelector(".webgl");
@@ -48,42 +143,13 @@ window.addEventListener("resize", () => {
 
 const scene = new THREE.Scene();
 
-const planeGeometry = new THREE.PlaneBufferGeometry(200, 200, 10, 17);
+const planeGeometry = new THREE.PlaneBufferGeometry(400, 400, 20, 20);
 const planeVertexCount = planeGeometry.attributes.position.count;
 
-const planeMaterial = new THREE.RawShaderMaterial({
-  vertexShader: `
-  uniform mat4 projectionMatrix;
-  uniform mat4 viewMatrix;
-  uniform mat4 modelMatrix;
-  
-  attribute vec3 position;
-  attribute float aFrequency;
-  varying float vFrequency;
-  void main()
-  {
-      
-      vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-      modelPosition.y += (aFrequency - 150.0) * 0.15;
-      vec4 viewPosition = viewMatrix * modelPosition;
-      vec4 projectedPosition = projectionMatrix * viewPosition;
-  
-      gl_Position = projectedPosition;
-  
-      vFrequency = aFrequency;
-  }`,
-  fragmentShader: `
-  precision mediump float;
-  
-  varying float vFrequency;
-  void main()
-  {
-  
-      gl_FragColor = vec4(1.0, vFrequency/350.0, ${
-        mode == "red" ? 0.0 : 1.0
-      }, 1.0);
-  }
-  `,
+const planeMaterial = new THREE.ShaderMaterial({
+  vertexShader: planeVertexShader,
+  uniforms: planeUniforms,
+  fragmentShader: planeFragmentShader,
   transparent: true,
   side: THREE.DoubleSide,
   wireframe: false,
@@ -99,43 +165,28 @@ scene.add(plane);
 
 // const sphereGeometry = new THREE.BoxGeometry(10,10,10, 2,2,2);
 
-const outerSphereGeometry = new THREE.IcosahedronGeometry(5, 1);
-const outerSphereMaterial = new THREE.RawShaderMaterial({
-  vertexShader: `
-  uniform mat4 projectionMatrix;
-  uniform mat4 viewMatrix;
-  uniform mat4 modelMatrix;
-  
-  attribute vec3 position;
-  attribute float aFrequency;
-  attribute float aFactor;
-  varying float vFrequency;
-  void main()
-  {
-      
-      vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-      if(aFrequency > 100.0){
-          modelPosition.y *= aFrequency * 0.01;
-      }
-      vec4 viewPosition = viewMatrix * modelPosition;
-      vec4 projectedPosition = projectionMatrix * viewPosition;
-  
-      gl_Position = projectedPosition;
-  
-      vFrequency = aFrequency * 1.0;
-  }`,
-  fragmentShader: `
-  precision mediump float;
-  
-  varying float vFrequency;
-  void main()
-  {
-  
-      gl_FragColor = vec4(1.0, vFrequency/350.0, ${
-        mode == "red" ? 0.0 : 1.0
-      }, 1.0);
-  }
-  `,
+const outerShapeGeometry = new THREE.IcosahedronGeometry(5, 1);
+const outerShapeMaterial = new THREE.ShaderMaterial({
+  uniforms: planeUniforms,
+  vertexShader: shapeVertexShader,
+  fragmentShader: shapeFragmentShader,
+  transparent: true,
+  side: THREE.DoubleSide,
+  wireframe: true,
+  // fog: true,
+  transparent: true,
+
+  // flatshading: true
+});
+
+const outerShape = new THREE.Mesh(outerShapeGeometry, outerShapeMaterial);
+outerShape.position.y = 5;
+scene.add(outerShape);
+
+const innerShapeGeometry = new THREE.IcosahedronGeometry(3, 2);
+const innerShapeMaterial = new THREE.ShaderMaterial({
+  vertexShader: shapeVertexShader,
+  fragmentShader: shapeFragmentShader,
   transparent: true,
   side: THREE.DoubleSide,
   wireframe: false,
@@ -144,58 +195,9 @@ const outerSphereMaterial = new THREE.RawShaderMaterial({
   // flatshading: true
 });
 
-const outerSphere = new THREE.Mesh(outerSphereGeometry, outerSphereMaterial);
-outerSphere.position.y = 5;
-scene.add(outerSphere);
-
-const innerSphereGeometry = new THREE.IcosahedronGeometry(3, 0);
-const innerSphereMaterial = new THREE.RawShaderMaterial({
-  vertexShader: `
-  uniform mat4 projectionMatrix;
-  uniform mat4 viewMatrix;
-  uniform mat4 modelMatrix;
-  
-  attribute vec3 position;
-  attribute float aFrequency;
-  attribute float aFactor;
-  varying float vFrequency;
-  void main()
-  {
-      
-      vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-      if(aFrequency > 100.0){
-          modelPosition.y *= aFrequency * 0.01;
-      }
-      vec4 viewPosition = viewMatrix * modelPosition;
-      vec4 projectedPosition = projectionMatrix * viewPosition;
-  
-      gl_Position = projectedPosition;
-  
-      vFrequency = aFrequency * 1.0;
-  }`,
-  fragmentShader: `
-  precision mediump float;
-  
-  varying float vFrequency;
-  void main()
-  {
-  
-      gl_FragColor = vec4(1.0, vFrequency/350.0, ${
-        mode == "red" ? 0.0 : 1.0
-      }, 1.0);
-  }
-  `,
-  transparent: true,
-  side: THREE.DoubleSide,
-  wireframe: false,
-  // fog: true,
-  transparent: true,
-  // flatshading: true
-});
-
-const innerSphere = new THREE.Mesh(innerSphereGeometry, innerSphereMaterial);
-innerSphere.position.y = 7;
-scene.add(innerSphere);
+const innerShape = new THREE.Mesh(innerShapeGeometry, innerShapeMaterial);
+innerShape.position.y = 5;
+scene.add(innerShape);
 
 const camera = new THREE.PerspectiveCamera(
   55,
@@ -221,18 +223,29 @@ scene.add(ambientLight);
 renderer.render(scene, camera);
 const controls = new OrbitControls(camera, renderer.domElement);
 
-let fogColor = new THREE.Color(mode);
+let fogColor = new THREE.Color("black");
 
 scene.background = fogColor;
 scene.fog = new THREE.Fog(fogColor, 0.25, 10);
 
 let clock = new THREE.Clock();
-function animate() {
+function animate(time) {
+  time *= 0.001;
+  const canvas = renderer.domElement;
+
+  planeUniforms.iResolution.value.set(canvas.width, canvas.height, 1);
+  planeUniforms.iTime.value = time;
   // console.log(frequencyData);
-  if (analyser) analyser.getByteFrequencyData(frequencyData);
+  if (analyser) {
+    console.log(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(frequencyData);
+  }
+
   // console.log(frequencyData)
   let max = 0;
   let min = 1000;
+  let j = 0;
+  let newFreqData = new Uint8Array(1024 * 3);
   for (let i = 0; i < frequencyData.length; i++) {
     if (frequencyData[i] > max) {
       max = frequencyData[i];
@@ -240,7 +253,11 @@ function animate() {
     if (frequencyData[i] < min) {
       min = frequencyData[i];
     }
+    newFreqData[j++] = frequencyData[i] + Math.random() / 4;
+    newFreqData[j++] = frequencyData[i] + Math.random() / 4;
+    newFreqData[j++] = frequencyData[i] + Math.random() / 4;
   }
+
   // console.log(max, min);
 
   planeGeometry.setAttribute(
@@ -248,12 +265,12 @@ function animate() {
     new THREE.BufferAttribute(frequencyData, 1)
   );
 
-  outerSphereGeometry.setAttribute(
+  outerShapeGeometry.setAttribute(
     "aFrequency",
     new THREE.BufferAttribute(frequencyData, 1)
   );
 
-  innerSphereGeometry.setAttribute(
+  innerShapeGeometry.setAttribute(
     "aFrequency",
     new THREE.BufferAttribute(frequencyData, 1)
   );
@@ -263,16 +280,16 @@ function animate() {
   plane.geometry.computeVertexNormals();
   plane.geometry.computeVertexNormals();
 
-  outerSphere.geometry.verticesNeedsUpdates = true;
-  outerSphere.geometry.normalNeedsUpdates = true;
-  outerSphere.geometry.computeVertexNormals();
-  outerSphere.geometry.computeVertexNormals();
+  outerShape.geometry.verticesNeedsUpdates = true;
+  outerShape.geometry.normalNeedsUpdates = true;
+  outerShape.geometry.computeVertexNormals();
+  outerShape.geometry.computeVertexNormals();
 
-  innerSphere.geometry.verticesNeedsUpdates = true;
-  innerSphere.geometry.normalNeedsUpdates = true;
-  innerSphere.geometry.computeVertexNormals();
-  innerSphere.geometry.computeVertexNormals();
-  outerSphere.rotation.y = clock.getElapsedTime() * 1.5;
+  innerShape.geometry.verticesNeedsUpdates = true;
+  innerShape.geometry.normalNeedsUpdates = true;
+  innerShape.geometry.computeVertexNormals();
+  innerShape.geometry.computeVertexNormals();
+  outerShape.rotation.y = clock.getElapsedTime() * 1.5;
 
   requestAnimationFrame(animate);
   controls.update();
@@ -280,31 +297,11 @@ function animate() {
 }
 
 animate();
-
-// const updateMode = () => {};
-
-// document.getElementById("change-btn").addEventListener("click", () => {
-//   // console.log("clicked", mode);
-//   if (mode === "red") {
-//     mode = "#ff00ff";
-//     updateMode();
-//   } else {
-//     mode = "red";
-//     updateMode();
-//   }
-// });
-
-// setInterval(() => {
-//   scene.background = new THREE.Color(
-//     `rgb(${Math.round(Math.random() * 300)},${Math.round(
-//       Math.random() * 300
-//     )},${Math.round(Math.random() * 300)})`
-//   );
-// }, 1500);
 document.getElementById("change-btn").addEventListener("click", () => {
-  // console.log("clicked", mode);
-  document.getElementById('test-song').src = `./Music/test-song${songNum++%4 + 1}.mp3`
-  console.log()
-  
+  document.getElementById("test-song").src = `./Music/test-song${
+    (songNum++ % 4) + 1
+  }.mp3`;
+  console.log();
+
   document.getElementById("test-song").play();
 });
